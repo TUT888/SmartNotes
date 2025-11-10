c00e4b5d6ea1b89911741b29362a4d341fa8a142
Tat Uyen Tam <tattam192@gmail.com>
2025-11-10 12:12:00 +1100

    refactor(note): change update note endpoint from PUT to PATCH

--- src/main/java/com/be08/smart_notes/controller/NoteController.java
+++ src/main/java/com/be08/smart_notes/controller/NoteController.java
@@ -7,14 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.be08.smart_notes.dto.request.NoteUpsertRequest;
 import com.be08.smart_notes.dto.response.ApiResponse;
@@ -59,7 +52,8 @@ public class NoteController {
         return ResponseEntity.status(HttpStatus.OK).body(apiResponse);
     }
 
-    @PutMapping("/{id}")
+    // @PutMapping("/{id}")
+    @PatchMapping("/{id}")
 	public ResponseEntity<Object> updateNote(@PathVariable int id, @RequestBody @Valid NoteUpsertRequest noteUpdateRequest) {
         NoteResponse note = noteService.updateNote(id, noteUpdateRequest);
 		ApiResponse<Object> apiResponse = ApiResponse.builder()
